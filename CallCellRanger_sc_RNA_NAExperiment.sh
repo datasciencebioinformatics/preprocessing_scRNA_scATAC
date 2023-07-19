f43d325c0a8e5f2f541931a7581ea5a9f0686d36
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2023-07-19 15:00:56 -0300

    Update CallCellRanger_sc_RNA_NAExperiment.sh

--- CallCellRanger_sc_RNA_NAExperiment.sh
+++ CallCellRanger_sc_RNA_NAExperiment.sh
@@ -2,32 +2,32 @@
 #### For each sample in the metadata, cellranger-atac count will be used 
 #### fastqs=
 #### reference=
-cat /home/cellranger/database/metadata.txt | grep -v "#"  | while read line 
+cat /home/cellranger/database/metadata.txt | grep -v "#"  | awk {'print $6'} | sort -u | uniq | while read unique_samples
 do
-  # Take the id
-  experiment=$(echo $line | awk {'print $1'})
-  
-  # Take the id
-  file=$(echo $line | awk -v directory=$database_folder {'print directory$2'})
-  
-  # Take the sample
-  sample=$(echo $line | awk {'print $3'})
-	
-  # Take the lane
-  lane=$(echo $line | awk {'print $4'})	
-	
-  # Take the sample
-  rep=$(echo $line | awk {'print $5'})		
-	
-  # Take the sample
-  direction=$(echo $line | awk {'print $6'})
-
-  # Take the transcriptome
-  transcriptome==$(echo $line | awk {'print $7'})
-	
-  # Print
-  echo -e $experiment" "$file"\t"$sample"\t"$lane"\t"$rep"\t"$direction	
-  
+	cat /home/cellranger/database/metadata.txt | grep $unique_samples | while read line
+ 	do
+		
+		# Take the id
+		experiment=$(echo $line | awk {'print $1'})
+		
+		# Take the id
+		file=$(echo $line | awk -v directory=$database_folder {'print directory$2'})
+		
+		# Take the sample
+		sample=$(echo $line | awk {'print $3'})
+		
+		# Take the lane
+		lane=$(echo $line | awk {'print $4'})	
+		
+		# Take the sample
+		rep=$(echo $line | awk {'print $5'})		
+		
+		# Take the sample
+		direction=$(echo $line | awk {'print $6'})
+		
+		# Take the transcriptome
+		transcriptome==$(echo $line | awk {'print $7'})
+  	done		
+	  # Print the command line
+	  echo /home/cellranger/cellranger-6.1.2/bin/cellranger /home/cellranger/cellranger-6.1.2/count --id=$experiment --transcriptome=$transcriptome --fastqs=/home/cellranger/database/hypothalamu_scRNA_scATAC/ --sample=$sample --localcores=8 --localmem=16   
 done
-  # Print the command line
-  echo /home/cellranger/cellranger-6.1.2/bin/cellranger /home/cellranger/cellranger-6.1.2/count --id=$experiment --transcriptome=$transcriptome --fastqs=/home/cellranger/database/hypothalamu_scRNA_scATAC/ --sample=$sample --localcores=8 --localmem=16
