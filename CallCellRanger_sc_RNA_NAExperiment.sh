bcc2067457a9539939be9196d0fb4dc6819e3bf2
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2023-07-19 16:56:38 -0300

    Update CallCellRanger_sc_RNA_NAExperiment.sh

--- CallCellRanger_sc_RNA_NAExperiment.sh
+++ CallCellRanger_sc_RNA_NAExperiment.sh
@@ -1,12 +1,22 @@
+# Save file for command
+command_file="/home/cellranger/database/command_file.txt"
+
+# Clean file for command
+rm -r $command_file
+
 ### Cellranger sc-ATAC command:
 #### For each sample in the metadata, cellranger-atac count will be used 
-#### fastqs=
-#### reference=
 cat /home/cellranger/database/metadata.txt | grep -v "#"  | awk {'print $6'} | sort -u | uniq | while read unique_samples
 do
-	cat /home/cellranger/database/metadata.txt | grep $unique_samples | while read line
- 	do
-		
+	experiment=""
+ 	file=""
+  	sample=""
+   	lane=""
+    	rep=""
+     	direction=""
+      	transcriptome=""
+	cat /home/cellranger/database/metadata.txt | grep $unique_samples | awk '{print $1"\t"$3"\t"$7}' | sort -u | while read line
+ 	do		
 		# Take the id
 		experiment=$(echo $line | awk {'print $1'})
 		
@@ -27,7 +37,9 @@ do
 		
 		# Take the transcriptome
 		transcriptome==$(echo $line | awk {'print $7'})
-  	done		
-	  # Print the command line
-	  echo /home/cellranger/cellranger-6.1.2/bin/cellranger /home/cellranger/cellranger-6.1.2/count --id=$experiment --transcriptome=$transcriptome --fastqs=/home/cellranger/database/hypothalamu_scRNA_scATAC/ --sample=$sample --localcores=8 --localmem=16   
-done
+   
+  	done		  
+	# Print the command line
+	echo /home/cellranger/cellranger-6.1.2/bin/cellranger /home/cellranger/cellranger-6.1.2/count --id=$experiment --transcriptome=$transcriptome --fastqs=/home/cellranger/database/hypothalamu_scRNA_scATAC/ --sample=$sample --localcores=8 --localmem=16
+
+   done
