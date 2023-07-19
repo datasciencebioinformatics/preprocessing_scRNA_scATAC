dd889a48aa01da1b05ec3b29feaa11e4ff419072
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2023-07-19 14:39:52 -0300

    Update CallCellRanger_sc_RNA_NAExperiment.sh

--- CallCellRanger_sc_RNA_NAExperiment.sh
+++ CallCellRanger_sc_RNA_NAExperiment.sh
@@ -5,20 +5,23 @@
 cat /home/cellranger/database/metadata.txt | grep -v "#"  | while read line 
 do
   # Take the id
-  file=$(echo $line | awk -v directory=$database_folder {'print directory$1'})
+  experiment=$(echo $line | awk {'print $1'})
+  
+  # Take the id
+  file=$(echo $line | awk -v directory=$database_folder {'print directory$2'})
   
   # Take the sample
-  sample=$(echo $line | awk {'print $2'})
+  sample=$(echo $line | awk {'print $3'})
 	
   # Take the lane
-  lane=$(echo $line | awk {'print $3'})	
+  lane=$(echo $line | awk {'print $4'})	
 	
   # Take the sample
-  rep=$(echo $line | awk {'print $4'})		
+  rep=$(echo $line | awk {'print $5'})		
 	
   # Take the sample
-  direction=$(echo $line | awk {'print $5'})			
+  direction=$(echo $line | awk {'print $6'})			
 	
   # Print
-  echo -e $file" "$sample"\t"$lane"\t"$rep"\t"$direction	
+  echo -e $experiment" "$file"\t"$sample"\t"$lane"\t"$rep"\t"$direction	
 done
